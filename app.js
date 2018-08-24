1abd52853e424863b06bf8df1db373384731b769
qlam699 <quanglam.thieu@yahoo.com>
2018-08-24 16:39:39 +0700

    add review with ReviewController

--- app.js
+++ app.js
@@ -15,25 +15,66 @@
         }
     });
 
+    app.controller("ReviewController", function() {
+        this.review = {};
+        this.addReview = function(product) {
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
+
     var products = [{
             images: [{ thumbnail: 'thumbnail.jpg', full: 'thumbnail.jpg' }],
             name: 'iPad Air 2 64G',
             price: 499,
             description: 'The newest Apple iPad 1.',
-            inStock: true
+            inStock: true,
+            reviews: [{
+                    stars: 5,
+                    body: "I love this product!",
+                    author: "qlam@gmail.com"
+                },
+                {
+                    stars: 1,
+                    body: "This product sucks",
+                    author: "bob@qlam.com"
+                }
+            ]
         },
         {
             images: [{ thumbnail: 'thumbnail.jpg', full: 'thumbnail.jpg' }],
             name: 'iPad Air 2 24G',
             price: 499,
             description: 'The newest Apple iPad 2.',
-            inStock: false
+            inStock: false,
+            reviews: [{
+                    stars: 5,
+                    body: "I love this product!",
+                    author: "qlam@gmail.com"
+                },
+                {
+                    stars: 1,
+                    body: "This product sucks",
+                    author: "bob@qlam.com"
+                }
+            ]
         }, {
             images: [{ thumbnail: 'thumbnail.jpg', full: 'thumbnail.jpg' }],
             name: 'iPad Air 2 124G',
             price: 499,
             description: 'The newest Apple iPad 3.',
-            inStock: true
+            inStock: true,
+            reviews: [{
+                    stars: 5,
+                    body: "I love this product!",
+                    author: "qlam@gmail.com"
+                },
+                {
+                    stars: 1,
+                    body: "This product sucks",
+                    author: "bob@qlam.com"
+                }
+            ]
         }
     ]
 })();
